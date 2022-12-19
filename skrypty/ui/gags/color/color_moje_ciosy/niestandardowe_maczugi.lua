26c6ed94c09263a79961b18177493f652bcbf38b
unknown <axesider@gmail.com>
2022-12-19 12:19:29 +0100

    poprawki dwusegemnetowej i mithryla

--- skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_maczugi.lua
+++ skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_maczugi.lua
@@ -7,13 +7,15 @@ function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_dwusegmentowa()
     local dmg = matches["damage"]
     local value = -1
         if dmg == "unika" then value = 0
-    elseif dmg == "nie zadajac" or dmg == "ledwie muskajac" then value = 1
+    elseif dmg == "nie zadajac" or dmg == "ledwie muskajac" or dmg =="broczaca" or dmg == "oszczednego" then value = 1
     elseif dmg == "broczaca krwia rane" or dmg == "nieduze obrazenia" then value = 2
     elseif dmg == "krwawo wgryzaja" or dmg == "widoczne obrazenia" then value = 3
-    elseif dmg == "raniac" then value = 4
-    elseif dmg == "paskudnie kaleczac" then value = 5
-    elseif dmg == "krwawo rozrywa" or dmg == "potwornie rani" then value = 6
+    elseif dmg == "raniac" or dmg == "z impetem" then value = 4
+    elseif dmg == "paskudnie kaleczac" or dmg == "celnie" then value = 5
+    elseif dmg == "krwawo rozrywa" or dmg == "potwornie rani" or dmg == "mocarnie" then value = 6
     elseif dmg == "masakrujac" then value = 7
+    else
+        debugc("trigger_func_skrypty_ui_gags_color_color_moje_ciosy_dwusegmentowa() unknown damage '"..dmg.."' ".. matches[1])
     end
     scripts.gags:gag(value, 7, target)
 end
