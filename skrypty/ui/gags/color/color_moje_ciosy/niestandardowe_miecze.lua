b050d919c6853f067b9fd2fd98cc61b8cfd953f2
unknown <axesider@gmail.com>
2023-02-25 22:33:11 +0100

    poprawki broni, wiedzminski, rapiery, polepszone regexpy

--- skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_miecze.lua
+++ skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_miecze.lua
@@ -101,3 +101,60 @@ function trigger_func_skrypty_ui_gags_ciosy_czarny_smukly_miecz()
     end
     scripts.gags:gag(value, 6, target)
 end
+
+-- Srebrny wiedzminski miecz dwureczny
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_ledwo_muska()
+    scripts.gags:gag(1, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_lekko_rani()
+    scripts.gags:gag(2, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_rani()
+    scripts.gags:gag(3, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_powaznie_rani()
+    scripts.gags:gag(4, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_bardzo_ciezko_rani()
+    scripts.gags:gag(5, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_masakruje()
+    scripts.gags:gag(6, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_wiedzminski_ja_masakruje1()
+    scripts.gags:gag(7, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_ciosy_srebrny_wiedzminski_miecz_dwureczny()
+    local target = "moje_ciosy"
+    if matches["attacker"] then
+        target = matches["target"] == "cie" and "innych_ciosy_we_mnie" or "innych_ciosy"
+    end
+    local dmg = matches["damage"]
+    local value = -1
+    scripts.gags:gag(value, 6, target)
+end
+
+-- Waski kunsztowny sihill
+function trigger_func_skrypty_ui_gags_ciosy_waski_kunsztowny_sihill()
+    local target = "moje_ciosy"
+    if matches["attacker"] then
+        target = (matches["target"] == "cie" or matches["target"] == "ciebie") and "innych_ciosy_we_mnie" or "innych_ciosy"
+    end
+
+    local dmg = matches["damage"]
+    local value = -1
+        if dmg == "obok" then value = 0
+    elseif dmg == "nieznaczne"  then value = 1
+    elseif dmg == "lekko raniac" then value = 2
+    elseif dmg == "wbija" then value = 4
+    end
+    scripts.gags:gag(value, 6, target)
+end
